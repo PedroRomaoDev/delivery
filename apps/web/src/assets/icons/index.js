d5d12bcf22070879c9ffe45a00a80160752ca944
PedroRomaoDev <pedro7ntj@gmail.com>
2026-02-19 17:29:36 -0300

    feat: add initial dashboard page with header components

--- apps/web/src/assets/icons/index.js
+++ apps/web/src/assets/icons/index.js
@@ -4,5 +4,14 @@ import PackageIcon from './package.svg?react';
 import PlusIcon from './plus.svg?react';
 import TasksIcon from './tasks.svg?react';
 import TruckIcon from './truck.svg?react';
+import XCircleIcon from './x-circle.svg?react';
 
-export { ArchiveIcon, HomeIcon, PackageIcon, PlusIcon, TasksIcon, TruckIcon };
+export {
+    ArchiveIcon,
+    HomeIcon,
+    PackageIcon,
+    PlusIcon,
+    TasksIcon,
+    TruckIcon,
+    XCircleIcon,
+};
